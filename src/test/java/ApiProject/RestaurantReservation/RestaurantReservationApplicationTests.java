4885308abd3bd770b49416b56dbbdc415f4488be
Wandersson Dutra <wanderssongds@gmail.com>
2025-06-22 23:55:38 -0300

    updates made int the SpringBoot test class and the application-test.properties

--- src/test/java/ApiProject/RestaurantReservation/RestaurantReservationApplicationTests.java
+++ src/test/java/ApiProject/RestaurantReservation/RestaurantReservationApplicationTests.java
@@ -2,20 +2,32 @@ package ApiProject.RestaurantReservation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
 
-
+@Testcontainers
 @SpringBootTest
-@ActiveProfiles("test")
-class RestaurantReservationApplicationTests {
+public class RestaurantReservationApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:15")
+			.withDatabaseName("testdb")
+			.withUsername("postgres")
+			.withPassword("postgres");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+		registry.add("spring.datasource.username", postgresContainer::getUsername);
+		registry.add("spring.datasource.password", postgresContainer::getPassword);
+		registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
